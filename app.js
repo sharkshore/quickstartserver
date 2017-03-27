87268b520f87140c16dfe785e17301de02384f1b
tuze <jiumozhi@baofu.com>
2017-03-27 15:41:56 +0800

    支持跨域

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors=require('cors');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -9,6 +10,8 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+//支持跨域
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,10 +28,62 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+
 //插入部分
-app.get('/json',function(req,res,next){   
-    res.jsonp({status:'json'});  
+//
+//
+app.use('/idcard/getIdCardTimeStatiscsInfo',function(req,res,next){   
+
+  //res.header('Access-Control-Allow-Origin', '*');
+  //res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  //res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+	console.log(req.query);
+	console.log(req.params);
+	console.log(req.body);
+    res.json(
+ [
+
+    {
+        date:'2017-07-07',
+        member: '宝付支付有限公司',
+        product: 2,
+        response_level: 7,
+        count: 6990
+    },
+    {
+        date:'2017-07-07',
+        member: '宝付支付有限公司',
+        product: 2,
+        response_level: 8,
+        count: 6875
+    },
+    {
+        date:'2017-07-07',
+        member: '宝付支付有限公司',
+        product: 2,
+        response_level: 9,
+        count: 6379
+    },
+
+ ]
+	);  
 });  
+
+
+app.use('/idcard/getAllMemberNames',function(req,res,next){   
+
+  //res.header('Access-Control-Allow-Origin', '*');
+  //res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  //res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+    res.json(
+		['鹏元征信xxxxxx', '前海征信xxxxx']
+		);  
+});  
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
